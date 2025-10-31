1a742b0f961fffb71caa637b7ca6fb4db51488d7
Arsalan Mateen <arsalanmateen.pk@gmail.com>
2025-10-31 13:05:00 +0500

    feat(auth): clear session cookies on signout

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -20,4 +20,9 @@ const signin = async (req, res) => {
   }
 };
 
-export default { signin };
+const signout = (req, res) => {
+  res.clearCookie("t");
+  return res.status(200).json({ message: "signed out" });
+};
+
+export default { signin, signout };
