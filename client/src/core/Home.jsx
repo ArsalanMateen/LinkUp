6f94a91c2c5a42bddbe1f959b524bbc1ed68e897
Arsalan Mateen <arsalanmateen.pk@gmail.com>
2025-11-28 11:06:00 +0500

    feat(navigation): introduce the home route

---
+++ client/src/core/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>LinkUp</h1>
+      <p>Share ideas and connect with the community.</p>
+    </main>
+  );
+}
